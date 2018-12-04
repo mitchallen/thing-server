35be41e99019106d4cb3b0d85f3f8618478a7c20
Mitch Allen <mitch.c.allen@gmail.com>
2018-12-03 21:49:08 -0500

    feat: moved static list router to its own modules

---
+++ src/static-list-router.js
@@ -0,0 +1,44 @@
+
+var listRouter = require('express').Router();
+
+module.exports.create = (spec) => {
+
+    spec = spec || {};
+
+    const appName = spec.appName || 'app';
+    const version = spec.version || '0.0.1';
+    const label = spec.label || 'objects';
+    const route = spec.router || '/api';
+    const list = spec.list|| [{id: 1, title: "TODO" }];
+
+    listRouter.get('/', function(req, res) {
+        res.json({ 
+            status: 'OK', 
+            app: appName, 
+            version: version,
+            route: route 
+        });   
+    });
+    
+    listRouter.get(`/${label}`, (req, res) => {
+        res.json( list );   
+    });
+    
+    listRouter.get(`/${label}/count`, (req, res) => {
+        res.json( { count: list.length } );   
+    });
+    
+    listRouter.get(`/${label}/:id`, (req, res) => {
+        // convert from 1 based to 0 based array
+        const id = parseInt(req.params.id) - 1;
+        console.log(id);
+        if( id < 0 || id >= list.length ) {
+            res.status( 404 ).send(`id ${req.params.id} out of range [1 - ${list.length}]`);
+        } else {
+            res.json( list[id] );
+        }   
+    });
+
+    return listRouter;
+
+};
