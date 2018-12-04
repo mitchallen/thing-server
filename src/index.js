25fc62b514f24f5a909aa678b14fbe4ac7885d6b
Mitch Allen <mitch.c.allen@gmail.com>
2018-12-03 22:08:00 -0500

    feat: removed commented out code, added console instructions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 const express = require('express'),
     app = express(),
-    // routerV1 = express.Router(),
     things = require('./data/v1/things.json'),
     staticListRouter = require('./static-list-router'),
-    port = process.env.PORT || 3000;
+    PORT = process.env.PORT || 3000;
 
 const APP_NAME = 'thing-server';
-const APP_VERSION = '1.0.1';    // TODO automate incrementing
+const APP_VERSION = '1.0.2';    // TODO automate incrementing
 
 const THINGS_ROUTER = '/v1';
 
@@ -20,7 +19,8 @@ let routerThings = staticListRouter.create({
     version: APP_VERSION,
     label: 'things',
     router: THINGS_ROUTER,
-    list: things
+    list: things,
+    port: PORT  // for console instructions
 });
 
 app.use( THINGS_ROUTER, routerThings );
@@ -29,27 +29,4 @@ app.get('/', function(req, res) {
     res.json({ status: 'OK', app: APP_NAME, version: APP_VERSION, route: "/" });   
 });
 
-// routerV1.get('/', function(req, res) {
-//     res.json({ status: 'OK', app: "thing-server", route: "/v1" });   
-// });
-
-// routerV1.get('/things', function(req, res) {
-//     res.json( things );   
-// });
-
-// routerV1.get('/things/count', function(req, res) {
-//     res.json( { count: things.length } );   
-// });
-
-// routerV1.get('/things/:id', function(req, res) {
-//     // convert from 1 based to 0 based array
-//     const id = parseInt(req.params.id) - 1;
-//     console.log(id);
-//     if( id < 0 || id >= things.length ) {
-//         res.status( 404 ).send(`id ${req.params.id} out of range [1 - ${things.length}]`);
-//     } else {
-//         res.json( things[id] );
-//     }   
-// });
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(PORT, () => console.log(`${APP_NAME}:${APP_VERSION} - listening on port ${PORT}!`))
